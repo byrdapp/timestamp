fb3a08d319fcd65b7f452c3a9864a1459bbc0fad
blixenkrone <sblixenkrone@gmail.com>
2020-03-06 13:53:05 +0100

    added seconds parsing

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Hrello
 
-## Time stamp is in UTC and must be an int64
+## Time stamp is by default in UTC and parses milliseconds (can be configured, see below). Input value must be an int64
 
 ### Run cmd
 
 $ go build && ./timestamp -value={int64}
+
+### For seconds
+
+$ go build && ./timestamp -value={int64} -epoch=seconds
--- timestamp.go
+++ timestamp.go
@@ -16,14 +16,20 @@ type Logger interface {
 var (
 	logger Logger = log.New(os.Stdout, "", 0)
 	t      int64
+	etype  string
 )
 
+type EpochType string
+
 const (
-	DKFormat = "02-01-2006T15:04:05Z07:00"
+	DKFormat           = "02-01-2006T15:04:05Z07:00"
+	Millis   EpochType = "millis"
+	Seconds  EpochType = "seconds"
 )
 
 func main() {
 	flag.Int64Var(&t, "value", 0, "parse a int64 (millis) timestamp")
+	flag.StringVar(&etype, "epoch", "millis", "parse a int64 (millis) timestamp")
 	flag.Parse()
 
 	logger.Printf("%v", t)
@@ -31,8 +37,18 @@ func main() {
 		flag.PrintDefaults()
 		log.Fatalln("value has zero or negative value")
 	}
-	stamp := Timestamp(time.Unix(0, t)).UTC()
-	logger.Printf("%v : %T", stamp, stamp)
+
+	var stamp Timestamp
+	switch EpochType(etype) {
+	case Millis:
+		stamp = ParseMillis(t)
+	case Seconds:
+		stamp = ParseSeconds(t)
+	default:
+		stamp = ParseMillis(t)
+	}
+
+	logger.Printf("%v : %T", stamp.Format(), stamp)
 	if stamp.IsZero() {
 		log.Fatalln("time has zero or wrong value")
 	}
@@ -54,6 +70,14 @@ func (t Timestamp) UnixNano() int64 {
 }
 func (t Timestamp) UTC() Timestamp {
 	return Timestamp(time.Time(t).UTC())
+}
+
+func ParseMillis(v int64) Timestamp {
+	return Timestamp(time.Unix(0, v*int64(time.Millisecond))).UTC()
+}
+
+func ParseSeconds(v int64) Timestamp {
+	return Timestamp(time.Unix(v, 0)).UTC()
 }
 
 func (t *Timestamp) UnmarshalJSON(b []byte) error {
