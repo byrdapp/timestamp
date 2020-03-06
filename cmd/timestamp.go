b9a2c69aedccb042b67a72ad72d2d2f9bc13f626
blixenkrone <sblixenkrone@gmail.com>
2020-03-06 15:54:20 +0100

    Update timestamp.go

--- cmd/timestamp.go
+++ cmd/timestamp.go
@@ -15,6 +15,5 @@ func main() {
 	flag.Int64Var(&t, "value", 0, "parse a int64 (millis) timestamp")
 	flag.StringVar(&etype, "epoch", "millis", "parse a int64 (millis) timestamp")
 	flag.Parse()
-
 	parser.Parse(t, etype)
 }
